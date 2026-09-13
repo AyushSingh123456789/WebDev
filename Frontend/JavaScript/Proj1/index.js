0fe59e1624146994020413d86d4fcbb7e6f6ee61
Ayush Singh <ayushpratap251@gmail.com>
2026-09-13 18:51:33 +0530

    welcome project

--- Frontend/JavaScript/Proj1/index.js
+++ Frontend/JavaScript/Proj1/index.js
@@ -7,9 +7,13 @@
 // increment the count variable when the button is clicked.
 // change the count-el in the HTML to reflect the new count.
 
-let countEl = document.getElementById("count-el") // pass in argument
+let countEl = document.getElementById("count-el") // pass in argument, 'document' is a data-type Object.
 let count = 0
 function increment() {
     count += 1
     countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
 }
---
+++ Frontend/JavaScript/Proj2/index.js
@@ -0,0 +1,8 @@
+let welcomeEl = document.getElementById("welcome-el")
+let naam = "Ayush"
+let greeting = "Welcome Back"
+welcomeEl.innerText = greeting + " " + naam
+
+let welcomeTo = document.getElementById("welcome-to")
+let text = "Continue your courses"
+welcomeTo.innerText = text
