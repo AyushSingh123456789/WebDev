6940437a2d0e2c05d8d01fba1b529603cd95a0e0
Ayush Singh <ayushpratap251@gmail.com>
2026-09-13 23:50:00 +0530

    first two javascript projects, passenger counter, and welcome greetings.

--- Frontend/JavaScript/Proj1/index.js
+++ Frontend/JavaScript/Proj1/index.js
@@ -8,12 +8,20 @@
 // change the count-el in the HTML to reflect the new count.
 
 let countEl = document.getElementById("count-el") // pass in argument, 'document' is a data-type Object.
+let saveEl = document.getElementById("save-el")
 let count = 0
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
+    // countEl.innerText = count
 }
 
 function save() {
-    console.log(count)
+    let countStr = count + " - "
+    // saveEl.innerText += countStr
+    saveEl.textContent += countStr // Better alternative of .innerText as it allows proper concatenation, anc more efficient.(MDN search)
+    countEl.textContent = 0
+    count = 0
+    // console.log(count)
+
 }
--- Frontend/JavaScript/Proj2/index.js
+++ Frontend/JavaScript/Proj2/index.js
@@ -1,7 +1,9 @@
+
 let welcomeEl = document.getElementById("welcome-el")
 let naam = "Ayush"
 let greeting = "Welcome Back"
 welcomeEl.innerText = greeting + " " + naam
+welcomeEl.innerText += " 👋"
 
 let welcomeTo = document.getElementById("welcome-to")
 let text = "Continue your courses"
